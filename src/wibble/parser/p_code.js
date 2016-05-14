9d120eeef89c98d53d04bde521386550bd57153d
Robey Pointer <robeypointer@gmail.com>
2016-05-14 18:34:50 -0500

    finish off p_code!

--- src/wibble/parser/p_code.js
+++ src/wibble/parser/p_code.js
@@ -1,20 +1,10 @@
 "use strict";
 
 import $ from "packrattle";
-import { linespace, toSpan } from "./p_common";
-import { expression, PExpr } from "./p_expr";
+import { commentspace, linespace, repeatSeparated, repeatSurrounded, toSpan } from "./p_common";
+import { symbolRef } from "./p_const";
+import { expression, PExpr, reference } from "./p_expr";
 import { compoundType, typedecl } from "./p_type";
-
-// p_common = require './p_common'
-// p_const = require './p_const'
-// p_expr = require './p_expr'
-//
-// blockOf = p_common.blockOf
-// internalSymbolRef = p_const.internalSymbolRef
-// symbolRef = p_const.symbolRef
-// SYMBOL_NAME = p_common.SYMBOL_NAME
-// toState = p_common.toState
-// typedecl = p_type.typedecl
 
 /*
  * parse code
@@ -22,7 +12,44 @@ import { compoundType, typedecl } from "./p_type";
 
 class PFunction extends PExpr {
   constructor(inType, outType, body, span) {
-    super(`Function(${inType ? inType.inspect() : "none"} -> ${outType ? outType.inspect() : "none"})`, span, [ body ]);
+    super(`function(${inType ? inType.inspect() : "none"} -> ${outType ? outType.inspect() : "none"})`, span, [ body ]);
+  }
+}
+
+class PLocal extends PExpr {
+  constructor(name, expr, mutable) {
+    super(mutable ? "make" : "let", name.span, [ name, expr ]);
+  }
+}
+
+class PLocals extends PExpr {
+  constructor(span, locals) {
+    super("locals", span, locals);
+  }
+}
+
+class PAssignment extends PExpr {
+  constructor(name, expr, span) {
+    super("assign", span, [ name, expr ]);
+  }
+}
+
+class POn extends PExpr {
+  constructor(receiver, expr, span) {
+    super("on", span, [ receiver, expr ]);
+  }
+}
+
+class PReturn extends PExpr {
+  constructor(expr, span) {
+    super("return", span, [ expr ]);
+  }
+}
+
+class PBlock extends PExpr {
+  constructor(codes, trailingComment, span) {
+    super("block", span, codes);
+    this.trailingComment = trailingComment;
   }
 }
 
@@ -42,40 +69,69 @@ export const func = $([
   return new PFunction(match[0], match[1] ? match[1][0] : null, match[3], match[2]);
 });
 
-// # preserve location of name
-// localName = pr(SYMBOL_NAME).onMatch (m, state) -> { name: m[0], state }
-//
-// localVal = pr([
-//   pr([ "mutable", linespace ]).optional([])
-//   localName
-//   linespace
-//   pr("=").commit().drop()
-//   linespace
-//   (-> expression)
-// ]).onMatch (m, state) ->
-//   { local: m[1], value: m[2], mutable: m[0].length > 0, state: m[1].state }
-//
-// assignment = pr([ localName, linespace, toState(":="), linespace, (-> expression) ]).onMatch (m, state) ->
-//   { assignment: m[0].name, value: m[2], state: m[1] }
-//
-// handlerReceiver = pr.alt(symbolRef, internalSymbolRef, compoundType).describe("symbol or parameters")
-//
-// handler = pr([ toState("on"), linespace, handlerReceiver, linespace, pr("->").drop(), whitespace, expression ]).onMatch (m, state) ->
-//   { on: m[1], handler: m[2], state: m[0] }
-//
-// returnEarly = pr([ toState("return"), linespace, expression ]).onMatch (m, state) ->
-//   { returnEarly: m[1], state: m[0] }
-//
-// code1 = pr.alt(localVal, assignment, handler, returnEarly, expression).onFail("Expected declaration or expression")
-//
-// code = pr([ whitespace, code1 ]).onMatch (m) -> m[0]
-//
-// codeBlock = blockOf(code).onMatch (m, state) ->
-//   rv = { code: m.items, state: state }
-//   if m.trailingComment? then rv.trailingComment = m.trailingComment
-//   rv
-//
-//
-// exports.code = code
-// exports.codeBlock = codeBlock
-// exports.functionx = functionx
+function localDeclaration(operator, mutable) {
+  return $([
+    reference.named("identifier"),
+    $.drop(linespace),
+    $.drop(operator),
+    $.drop(linespace),
+    () => expression
+  ]).map(match => {
+    return new PLocal(match[0], match[1], mutable);
+  });
+}
+
+const localLet = $([
+  toSpan("let"),
+  repeatSeparated(localDeclaration("=", false), ",", $.drop(linespace))
+]).map(match => {
+  return new PLocals(match[0], match[1]);
+});
+
+const localMake = $([
+  toSpan("make"),
+  repeatSeparated(localDeclaration(":=", true), ",", $.drop(linespace))
+]).map(match => {
+  return new PLocals(match[0], match[1]);
+});
+
+const assignment = $([
+  reference,
+  $.drop(linespace),
+  toSpan(":="),
+  $.drop(linespace),
+  () => expression
+]).map(match => {
+  return new PAssignment(match[0], match[2], match[1]);
+});
+
+const handlerReceiver = $.alt(() => symbolRef, () => compoundType).named("symbol or parameters");
+const handler = $([
+  toSpan("on"),
+  $.drop(linespace),
+  handlerReceiver,
+  $.drop(linespace),
+  $.drop("->"),
+  $.drop(linespace),
+  () => expression
+]).map(match => {
+  return new POn(match[1], match[2], match[0]);
+});
+
+const returnEarly = $([ toSpan("return"), $.drop(linespace), () => expression ]).map(match => {
+  return new PReturn(match[1], match[0]);
+});
+
+export const code = $.alt(
+  localLet,
+  localMake,
+  assignment,
+  handler,
+  returnEarly,
+  () => expression
+).named("declaration or expression");
+
+const lf = $(/[\n;]+/).named("linefeed or ;");
+export const codeBlock = repeatSurrounded($.commit("{"), code, lf, "}", commentspace).map((match, span) => {
+  return new PBlock(match[0], match[1], span);
+});
--- test/src/parser/test_p_code.js
+++ test/src/parser/test_p_code.js
@@ -7,30 +7,19 @@ import "source-map-support/register";
 
 const parse = (s, options) => parser.func.run(s, options).inspect();
 const parseExpr = (s, options) => parser.expression.run(s, options).inspect();
+const parseCode = (s, options) => parser.code.run(s, options).inspect();
+const parseBlock = (s, options) => parser.codeBlock.run(s, options).inspect();
 
-// should = require 'should'
-// util = require 'util'
-//
-// wibble = "../lib/wibble"
-// p_code = require "#{wibble}/parser/p_code"
-// p_expr = require "#{wibble}/parser/p_expr"
-// test_util = require './test_util'
-//
-// parseWith = test_util.parseWith
-// parseFailedWith = test_util.parseFailedWith
-//
+
 describe("Parse code", () => {
   describe("function", () => {
-//     parse = (line, options) -> parseWith(p_code.functionx, line, options)
-//     parseFailed = (line, options) -> parseFailedWith(p_code.functionx, line, options)
-//
     it("empty", () => {
-      parse("-> ()").should.eql("Function(none -> none)(const(NOTHING)[3:5])[0:2]");
+      parse("-> ()").should.eql("function(none -> none)(const(NOTHING)[3:5])[0:2]");
     });
 
     it("simple expression", () => {
       parse("(x: Int) -> x * 2").should.eql(
-        "Function(CompoundType(Field(x: Type(Int)[4:7])[1:2])[0:8] -> none)(" +
+        "function(compoundType(field(x: type(Int)[4:7])[1:2])[0:8] -> none)(" +
           "binary(*)(x[12:13], const(NUMBER_BASE10, 2)[16:17])[12:17]" +
         ")[9:11]"
       );
@@ -38,7 +27,7 @@ describe("Parse code", () => {
 
     it("with no arg type", () => {
       parse("(x) -> x * 2").should.eql(
-        "Function(CompoundType(Field(x)[1:2])[0:3] -> none)(" +
+        "function(compoundType(field(x)[1:2])[0:3] -> none)(" +
           "binary(*)(x[7:8], const(NUMBER_BASE10, 2)[11:12])[7:12]" +
         ")[4:6]"
       );
@@ -46,7 +35,7 @@ describe("Parse code", () => {
 
     it("with return type", () => {
       parse("(x: Int): Int -> x").should.eql(
-        "Function(CompoundType(Field(x: Type(Int)[4:7])[1:2])[0:8] -> Type(Int)[10:13])(" +
+        "function(compoundType(field(x: type(Int)[4:7])[1:2])[0:8] -> type(Int)[10:13])(" +
           "x[17:18]" +
         ")[14:16]"
       );
@@ -54,11 +43,11 @@ describe("Parse code", () => {
 
     it("complex parameters", () => {
       parse("(a: Map(String, Int), b: String -> Int) -> false").should.eql(
-        "Function(CompoundType(" +
-          "Field(a: TemplateType(Map)(" +
-            "Type(String)[8:14], Type(Int)[16:19]" +
+        "function(compoundType(" +
+          "field(a: templateType(Map)(" +
+            "type(String)[8:14], type(Int)[16:19]" +
           ")[4:20])[1:2], " +
-          "Field(b: FunctionType(Type(String)[25:31], Type(Int)[35:38])[25:38])[22:23]" +
+          "field(b: functionType(type(String)[25:31], type(Int)[35:38])[25:38])[22:23]" +
         ")[0:39] -> none)(" +
           "const(BOOLEAN, false)[43:48]" +
         ")[40:42]"
@@ -67,9 +56,9 @@ describe("Parse code", () => {
 
     it("default values", () => {
       parse("(x: Int = 4, y: Int = 5) -> x + y").should.eql(
-        "Function(CompoundType(" +
-          "Field(x: Type(Int)[4:7] = const(NUMBER_BASE10, 4)[10:11])[1:2], " +
-          "Field(y: Type(Int)[16:19] = const(NUMBER_BASE10, 5)[22:23])[13:14]" +
+        "function(compoundType(" +
+          "field(x: type(Int)[4:7] = const(NUMBER_BASE10, 4)[10:11])[1:2], " +
+          "field(y: type(Int)[16:19] = const(NUMBER_BASE10, 5)[22:23])[13:14]" +
         ")[0:24] -> none)(" +
           "binary(+)(x[28:29], y[32:33])[28:33]" +
         ")[25:27]"
@@ -78,122 +67,85 @@ describe("Parse code", () => {
 
     it("nested", () => {
       parse("-> -> 69").should.eql(
-        "Function(none -> none)(Function(none -> none)(const(NUMBER_BASE10, 69)[6:8])[3:5])[0:2]"
+        "function(none -> none)(function(none -> none)(const(NUMBER_BASE10, 69)[6:8])[3:5])[0:2]"
       );
     });
 
     it("via expression", () => {
       parseExpr("-> 3").should.eql(
-        "Function(none -> none)(const(NUMBER_BASE10, 3)[3:4])[0:2]"
+        "function(none -> none)(const(NUMBER_BASE10, 3)[3:4])[0:2]"
       );
       parseExpr("(x: Int) -> 3").should.eql(
-        "Function(CompoundType(Field(x: Type(Int)[4:7])[1:2])[0:8] -> none)(const(NUMBER_BASE10, 3)[12:13])[9:11]"
+        "function(compoundType(field(x: type(Int)[4:7])[1:2])[0:8] -> none)(const(NUMBER_BASE10, 3)[12:13])[9:11]"
       );
     });
   });
 
-//   describe "code", ->
-//     parse = (line, options) -> parseWith(p_code.code, line, options)
-//     parseFailed = (line, options) -> parseFailedWith(p_code.code, line, options)
-//
-//     it "expression", ->
-//       parse("x + y").should.eql(
-//         binary: "+"
-//         left: { reference: "x", pos: [ 0, 1 ] }
-//         right: { reference: "y", pos: [ 4, 5 ] }
-//         pos: [ 0, 5 ]
-//       )
-//
-//     it "local val", ->
-//       parse("x = 100").should.eql(
-//         local: { name: "x", pos: [ 0, 1 ] }
-//         value: { number: "base10", value: "100", pos: [ 4, 7 ] }
-//         mutable: false
-//         pos: [ 0, 1 ]
-//       )
-//       parse("mutable count = 3").should.eql(
-//         local: { name: "count", pos: [ 8, 13 ] }
-//         value: { number: "base10", value: "3", pos: [ 16, 17 ] }
-//         mutable: true
-//         pos: [ 8, 13 ]
-//       )
-//
-//     it "assignment", ->
-//       parse("count := 9").should.eql(
-//         assignment: "count"
-//         value: { number: "base10", value: "9", pos: [ 9, 10 ] }
-//         pos: [ 6, 8 ]
-//       )
-//       parse("count := count + 1").should.eql(
-//         assignment: "count"
-//         value:
-//           binary: "+"
-//           left: { reference: "count", pos: [ 9, 14 ] }
-//           right: { number: "base10", value: "1", pos: [ 17, 18 ] }
-//           pos: [ 9, 18 ]
-//         pos: [ 6, 8 ]
-//       )
-//
-//     it "handler", ->
-//       parse("on .peek -> 3").should.eql(
-//         on: { symbol: "peek", pos: [ 3, 8 ] }
-//         handler: { number: "base10", value: "3", pos: [ 12, 13 ] }
-//         pos: [ 0, 2 ]
-//       )
-//       parse("on () -> true").should.eql(
-//         on: { compoundType: [], pos: [ 3, 5 ] }
-//         handler: { boolean: true, pos: [ 9, 13 ] }
-//         pos: [ 0, 2 ]
-//       )
-//       parse("on (x: Int) -> x * 2").should.eql(
-//         on:
-//           compoundType: [
-//             { name: "x", type: { typename: "Int", pos: [ 7, 10 ] }, value: undefined, pos: [ 4, 5 ] }
-//           ]
-//           pos: [ 3, 11 ]
-//         handler:
-//           binary: "*"
-//           left: { reference: "x", pos: [ 15, 16 ] }
-//           right: { number: "base10", value: "2", pos: [ 19, 20 ] }
-//           pos: [ 15, 20 ]
-//         pos: [ 0, 2 ]
-//       )
-//
-//     it "handler error", ->
-//       parseFailed("on 3 -> 3").should.match /symbol or parameters/
-//
-//     it "return", ->
-//       parse("return 3").should.eql(
-//         returnEarly: { number: "base10", value: "3", pos: [ 7, 8 ] }
-//         pos: [ 0, 6 ]
-//       )
-//
-//   describe "block of code", ->
-//     parse = (line, options) -> parseWith(p_code.codeBlock, line, options)
-//     parseFailed = (line, options) -> parseFailedWith(p_code.codeBlock, line, options)
-//
-//     it "empty", ->
-//       parse("{}").should.eql(code: [], pos: [ 0, 2 ])
-//       parse("{  }").should.eql(code: [], pos: [ 0, 4 ])
-//
-//     it "separated by ;", ->
-//       parse("{ 3; 4 }").should.eql(
-//         code: [
-//           { number: "base10", value: "3", pos: [ 2, 3 ] }
-//           { number: "base10", value: "4", pos: [ 5, 6 ] }
-//         ]
-//         pos: [ 0, 8 ]
-//       )
-//
-//     it "separated by linefeed", ->
-//       parse("{\n  true\n  false\n}").should.eql(
-//         code: [
-//           { boolean: true, pos: [ 4, 8 ] }
-//           { boolean: false, pos: [ 11, 16 ] }
-//         ]
-//         pos: [ 0, 18 ]
-//       )
-//
+  describe("code", () => {
+    it("expression", () => {
+      parseCode("x + y").should.eql("binary(+)(x[0:1], y[4:5])[0:5]");
+    });
+
+    it("let", () => {
+      parseCode("let x = 100").should.eql("locals(let(x[4:5], const(NUMBER_BASE10, 100)[8:11])[4:5])[0:3]");
+      (() => parseCode("let return = 1")).should.throw(/Reserved/);
+    });
+
+    it("make", () => {
+      parseCode("make x := 100").should.eql("locals(make(x[5:6], const(NUMBER_BASE10, 100)[10:13])[5:6])[0:4]");
+      (() => parseCode("make return = 1")).should.throw(/Reserved/);
+    });
+
+    it("assignment", () => {
+      parseCode("count := 9").should.eql("assign(count[0:5], const(NUMBER_BASE10, 9)[9:10])[6:8]");
+      parseCode("count := count + 1").should.eql(
+        "assign(count[0:5], binary(+)(count[9:14], const(NUMBER_BASE10, 1)[17:18])[9:18])[6:8]"
+      );
+    });
+
+    it("handler", () => {
+      parseCode("on .peek -> 3").should.eql(
+        "on(const(SYMBOL, peek)[3:8], const(NUMBER_BASE10, 3)[12:13])[0:2]"
+      );
+      parseCode("on () -> true").should.eql(
+        "on(compoundType[3:5], const(BOOLEAN, true)[9:13])[0:2]"
+      );
+      parseCode("on (x: Int) -> x * 2").should.eql(
+        "on(" +
+          "compoundType(field(x: type(Int)[7:10])[4:5])[3:11], " +
+          "binary(*)(x[15:16], const(NUMBER_BASE10, 2)[19:20])[15:20]" +
+        ")[0:2]"
+      );
+      (() => parseCode("on 3 -> 3")).should.match(/symbol or parameters/);
+    });
+
+    it("return", () => {
+      parseCode("return 3").should.eql(
+        "return(const(NUMBER_BASE10, 3)[7:8])[0:6]"
+      );
+    });
+  });
+
+  describe("block of code", () => {
+    it("empty", () => {
+      parseBlock("{}").should.eql("block[0:2]");
+      parseBlock("{  }").should.eql("block[0:4]");
+    });
+
+    it("separated by ;", () => {
+      parseBlock("{ 3; 4 }").should.eql("block(" +
+        "const(NUMBER_BASE10, 3)[2:3], " +
+        "const(NUMBER_BASE10, 4)[5:6]" +
+      ")[0:8]");
+    });
+
+    it("separated by linefeed", () => {
+      parseBlock("{\n  true\n  false\n}").should.eql("block(" +
+        "const(BOOLEAN, true)[4:8], " +
+        "const(BOOLEAN, false)[11:16]" +
+      ")[0:18]");
+    });
+
 //     it "comments", ->
 //       parse("{  # code\n# 3\n  true\n  # ok!\n}").should.eql(
 //         code: [
@@ -202,4 +154,5 @@ describe("Parse code", () => {
 //         trailingComment: "# ok!"
 //         pos: [ 0, 30 ]
 //       )
+  });
 });
