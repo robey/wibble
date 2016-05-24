375e49d768779f09b3e9eeb1e5a2b5fbcab53611
Robey Pointer <robeypointer@gmail.com>
2016-05-24 09:41:07 -0700

    stop here.

--- src/wibble/compiler/typecheck.js
+++ src/wibble/compiler/typecheck.js
@@ -1,9 +1,18 @@
 "use strict";
 
+import { transformAst } from "../common/transform";
+
 /*
  * 1. Attach a new (locals) scope to each block and handler.
- * 2.
+ * 2. Attach an unknown-type reference to each local and handler.
+ * 3. Catch unresolved references and duplicate names.
  */
-export function buildScopes(expr, state) {
-
+export function buildScopes(expr, errors) {
+  expr; errors;
 }
+
+/*
+ * unresolved:
+ *   - alias type "@" to the nearest outer "new"
+ *   - when a type is explicitly given, verify that it matches our inferred type.
+ */
