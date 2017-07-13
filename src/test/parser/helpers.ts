16bf985a5c3f172301405e970d54c9dfae10365b
Robey Pointer <robeypointer@gmail.com>
2017-07-13 11:59:51 -0700

    new helper.

--- src/test/parser/helpers.ts
+++ src/test/parser/helpers.ts
@@ -1,5 +1,10 @@
 import * as fs from "fs";
+import { Parser } from "packrattle";
 
 export function makeDot(s: string) {
   fs.writeFileSync("debug.dot", Buffer.from(s));
+}
+
+export function saveParser<A, Out>(p: Parser<A, Out>) {
+  fs.writeFileSync("parser.dot", Buffer.from(p.toDot()));
 }
